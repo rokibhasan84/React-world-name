14210bb6a9a5db912375e79be4331306a2302198
My Tablet <mttablettab438@gmail.com>
2025-09-19 23:40:53 +0800

    try to commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 
+import { Suspense } from 'react'
 import './App.css'
 import Countries from './components/Countries/Countries'
+
+
+
+const countriesPromise = fetch('https://openapi.programming-hero.com/api/all')
+  .then(res => res.json())
 
 function App() {
   
 
   return (
     <> 
-    <Countries></Countries>
+    <Suspense fallback={<p>Countries name loading......</p>}>
+      <Countries countriesPromise={countriesPromise}></Countries>
+    </Suspense>
       
       
     </>
---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Country = ({country}) => {
+console.log(country.population.population);
+
+    return (
+        <div>
+            <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+            <h3>Name: {country.name.common} </h3>
+            <p>Population: {country.population.population}</p>
+        </div>
+    );
+};
+
+export default Country;
